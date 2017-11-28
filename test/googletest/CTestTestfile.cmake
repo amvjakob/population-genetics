52579b7d50758b70b2726b8ea71c90f72cd00547
Jakob Anthony Maxime Victor <amjakob@SVBBCF20170052.intranet.epfl.ch>
2017-11-28 03:24:46 +0100

    Gitignore

--- test/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/INTRANET/lbruchez/myfiles/GeneticsProject/github/Team_4/test/googletest
-# Build directory: /home/INTRANET/lbruchez/myfiles/GeneticsProject/github/Team_4/test/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
